74995009afe85c3582f2d9d9d4f66bba64adbc02
khizarAhmed443 <128354019+khizarAhmed443@users.noreply.github.com>
2023-04-19 23:37:22 +0500

    App: Add feature to show recipe steps

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
-const apiKey = "f621548388724d2f92ed07915191481f";
-const id = 1003464;
-const ingredientApi = `https://api.spoonacular.com/recipes/${id}/ingredientWidget.json?apiKey=${apiKey}`;
-const allRecipes = `https://api.spoonacular.com/recipes/complexSearch?number=100&apiKey=${apiKey}`;
+const apiKey = "a2de8100073f49a382d99d5dc12e3062";
+const allRecipes = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}`;
 // const recipe = `https://api.spoonacular.com/recipes/${id}/information?apiKey=${apiKey}`;
 
 const card_container = document.getElementById("card-container");
@@ -17,18 +15,65 @@ const homeBtn = document
 //call showRecipes function to display recipes data when screen load.
 showRecipes();
 
-function showRecipes() {
+async function showRecipes() {
+  //null the cards div innerhtml
   card_container.innerHTML = "";
-  if (localStorage.getItem("Recipe") === null) {
-    fetch(allRecipes)
+
+  //check if api hit already than not hit again
+  if (localStorage.getItem("Recipes") === null) {
+    //   // fetch all recipes
+    await fetch(allRecipes)
       .then((response) => response.json())
-      .then((data) => localStorage.setItem("Recipe", JSON.stringify(data)))
+      .then((data) => localStorage.setItem("Recipes", JSON.stringify(data)))
       .catch((error) => console.error(error));
   }
-  getIngredient();
-  // console.log(ingredient);
-  let data = JSON.parse(localStorage.getItem("Recipe"));
+  // get data that store in local storage geting from API
+  let data = JSON.parse(localStorage.getItem("Recipes"));
   addCards(data);
+}
+async function addIngredients(data, localStorageName) {
+  if (localStorage.getItem(localStorageName) === null) {
+    for (let index = 0; index < data.results.length; index++) {
+      let element = data.results;
+
+      const ingredientApi = `https://api.spoonacular.com/recipes/${element.id}/ingredientWidget.json?apiKey=${apiKey}&number=10`;
+      await fetch(ingredientApi)
+        .then((response) => response.json())
+        .then((data) =>
+          localStorage.setItem(element.title, JSON.stringify(data))
+        )
+        .catch((error) => console.error(error));
+
+      //get ingredients data store in local storage of specific id
+      let ingredentsarray = [];
+      let ingredentsdata = JSON.parse(localStorage.getItem(element.title));
+      for (let index = 0; index < ingredentsdata.ingredients.length; index++) {
+        //object to store ingredients
+        let ingredents = {};
+        //get required items from api and store in ingredients hash table/object
+        ingredents.name = ingredentsdata.ingredients[index].name;
+        if (ingredentsdata.ingredients[index].amount.metric.unit == "") {
+          ingredents.unit = "qty";
+        } else {
+          ingredents.unit =
+            ingredentsdata.ingredients[index].amount.metric.unit;
+        }
+        ingredents.amount =
+          ingredentsdata.ingredients[index].amount.metric.value;
+        //add items objects to array of items/ingredients
+        ingredentsarray.push(ingredents);
+      }
+
+      //add ingredients data into recipes data
+      element.ingredients = [];
+
+      element.ingredients = ingredentsarray;
+    }
+
+    localStorage.setItem(localStorageName, JSON.stringify(data));
+  }
+  let Sdata = JSON.parse(localStorage.getItem(localStorageName));
+  addCards(Sdata);
 }
 
 //function to display cards on screen
@@ -42,45 +87,78 @@ function addCards(data) {
     <img src="${element.image}" alt="Food Recipe Image">
     <div class="card-body">
       <h3 class="card-title">${element.title}</h3>
-      <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris convallis libero augue, non aliquam odio pharetra non. Sed porttitor metus eget neque varius auctor. Nullam eu dui turpis.</p>
-      <a href="#" class="btn btn-primary">View Recipe</a>
+      
       </div>
       `;
     card_container.appendChild(card);
   });
 }
 
-//Function to display search recipes
+// Function to display search recipes
 async function searchRecipeFunction() {
   card_container.innerHTML = "";
   var search = searchBar.value.toLowerCase();
-  const searchRecipe = `https://api.spoonacular.com/recipes/complexSearch?number=100&query=${search}&apiKey=${apiKey}`;
-  if (localStorage.getItem(search) === null) {
+
+  if (localStorage.getItem("search") === null) {
+    const searchRecipe = `https://api.spoonacular.com/recipes/complexSearch?number=10&query=${search}&apiKey=${apiKey}`;
     await fetch(searchRecipe)
       .then((response) => response.json())
       .then((data) => localStorage.setItem(search, JSON.stringify(data)))
       .catch((error) => console.error(error));
   }
-
-  let data = JSON.parse(localStorage.getItem(search));
-  console.log(data);
-  card_container.innerHTML = "";
-
-  addCards(data);
+  let Sdata = JSON.parse(localStorage.getItem(search));
+  addCards(Sdata);
 }
 
+let checkrecipe = document.getElementsByClassName("card");
+// checkrecipe is object that is being converted into array
+const array = Array.from(checkrecipe);
+array.forEach(function (elem) {
+  elem.addEventListener("click", () => {
+    showrecipe(elem.id);
+  });
+});
 
+async function showrecipe(id) {
+  let card = document.getElementById("recipe-card-body");
+  card.innerHTML = "";
+  document.getElementById("recipe-card-container").style.display = "flex";
+  var recipe = `https://api.spoonacular.com/recipes/${id}/information?apiKey=${apiKey}`;
+  if (localStorage.getItem(id) === null) {
+    await fetch(recipe)
+      .then((response) => response.json())
+      .then((data) => localStorage.setItem(id, JSON.stringify(data)))
+      .catch((error) => console.error(error));
+  }
 
-function getIngredient(){
-  let getData = {};
-  fetch(ingredientApi)
-    .then((response) => response.json())
-    .then((data) => {for (let index = 0; index < data.ingredients.length; index++) {
-      console.log(data.ingredients[index].name);
-      
-    }
-      
-    })
-    .catch((error) => console.error(error));
-    
+  var recipedata = JSON.parse(localStorage.getItem(id));
+  // analyzedInstructions
+
+  var allSteps = recipedata.analyzedInstructions[0].steps;
+  allSteps.forEach(function (elem) {
+    card.innerHTML += `<p> ${elem.step}</p>`;
+  });
+}
+// Function to hide the card
+function hideCard() {
+  document.getElementById("recipe-card-container").style.display = "none";
+}
+
+{
+  /*  <div>
+ <h3 style="text-align:center">Ingredients</h3>
+<table id="myTable"style="text-align:left" ><thead><tr><th>Name</th><th>Volume</th><th>Unit</th></tr></thead><tbody> ${element.ingredients
+  .map(function (item) {
+    return (
+      "<tr><td>" +
+      item.name +
+      "</td><td>" +
+      item.amount +
+      "</td><td>" +
+      item.unit +
+      "</td></tr>"
+    );
+  })
+  .join("")} </tbody></table>
+  </div> */
 }
